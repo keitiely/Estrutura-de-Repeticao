4ab42be3757b780a98a234fa8ba97945ffbbe075
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-14 20:52:46 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -1,26 +1,23 @@
+/*Questao 4-Faça um programa que leia um valor N qualquer, inteiro e positivo, calcule e mostre a seguinte soma:*/
 #include <stdio.h>
-#include <math.h>
-
 int main(){
-	int vezes, i;
-	float debaixo=1, fatorial=1;
-	float x, pi;
-	
-	printf("Digite as vezes que o programa vai rodar (N): ");
-	scanf("%d", &vezes);
-	
-		for(i=vezes; i>=1; i--){
-    	fatorial *= i;
-    	
-    	debaixo+= 1.0/fatorial;
-    	}	
-		
-		
-		
-		printf("%f", debaixo);
-	
-	
-	
-	 //+ou- 188000 vezes
-	return 0;
+printf("\n\nProgama Soma de E: \n");
+
+int vezes, i, j;
+float soma = 1.0;
+
+printf("\nDigite as vezes que o programa vai rodar: ");
+scanf("%d", &vezes);
+
+for(i = 1; i <= vezes; i++){
+    int fatorial = 1.0;
+
+    for(j = 1; j <= i; j++){
+        fatorial = j;
+    }
+
+    soma += 1.0/fatorial;
+}
+
+printf("\n\tO valor da soma e igual a %.4f", soma);
 }
