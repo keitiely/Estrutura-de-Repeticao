474291aae3e497ccbdc8eb07f34b4b3f4093b4ea
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-27 21:40:17 -0300

    Update primo.c

--- primo.c
+++ primo.c
@@ -1,33 +1,17 @@
 /*8- Faça um aplicativo que verifique se o número digitado n é um número primo. 
 Lembrando que todo número primeiro é divisível apenas por 1 ou por ele mesmo.*/
-    #include <stdio.h>
-    #include <math.h>
+#include <stdio.h>
 
-    int main(){
-        int n;
-        int primo = 1;
+int main(){	
+    int num;
+	printf("Entre com um número: ");
+	scanf("%d", &num);
 
-        printf("Digite um numero: ");
-        scanf("%d", &n);
-
-        if (n <= 1) {
-            printf("O numero %d nao e primo\n", n);
-            primo=0;
-        } 
-        else{
-            for (int i = 2; i <= sqrt(n); i++) {
-                if (n % i == 0) {
-                    printf("O numero %d nao e primo\n", n);
-                    primo=0;
-                    break;
-                }
-            }
+	for(int i=2; i<num; i++){
+		if(num%i==0){
+            printf("%d não é primo!", num);
+			return 1;
         }
-        if(primo){
-            printf("O numero %d e primo\n", n);
-        }
-
-
-    }
-
-
+	}
+    printf("%d é primo!", num);
+}
