00b4875e6d7ed530792a406dae3c040f4009ed4f
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-27 23:02:30 -0300

    Update and rename Serie-Infinita to Serie-Infinita.c

--- Serie-Infinita.c
+++ Serie-Infinita.c
@@ -1,4 +1,4 @@
-/*Questao 3- Calcule o valor de a partir de uma série infinita
+/* Questao 3- Calcule o valor de a partir de uma série infinita
 	π = 4− 4/3 + 4/5 − 4/7 + 4/9 − 4/11 +... 
 Imprima uma tabela que mostre o valor aproximado de Pi calculando os 200.000 primeiros termos dessa série.
 Quantos termos você tem de utilizar antes de primeiro obter um valor que começa com 3,14159?*/
