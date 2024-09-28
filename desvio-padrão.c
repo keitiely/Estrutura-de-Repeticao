be9c6b8576ce9488e4e174e0b9e599b34e5d21f1
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-27 22:34:59 -0300

    Update desvio-padrão.c

--- desvio-padrão.c
+++ desvio-padrão.c
@@ -42,3 +42,37 @@ desvio_padrao = sqrt(somatorio_normal/(n_tamanho-1));
 
 printf("\nDesvio padrao %f", desvio_padrao);
 }
+
+
+
+/* 
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float x1, x2, x3, x4, x5, media, desvio_padrao;
+
+	printf("Entre com o valor de x1: ");
+	scanf("%f", &x1);
+
+	printf("Entre com o valor de x1: ");
+	scanf("%f", &x2);
+
+	printf("Entre com o valor de x1: ");
+	scanf("%f", &x3);
+	
+	printf("Entre com o valor de x1: ");
+	scanf("%f", &x4);
+
+	printf("Entre com o valor de x1: ");
+	scanf("%f", &x5);
+
+
+	media = (x1+x2+x3+x4+x5)/5;
+
+	desvio_padrao = sqrt((pow((x1-media),2)+pow((x2-media),2)+pow((x3-media),2)+pow((x4-media),2)+pow((x5-media),2))/4);
+
+	printf("A media é: %f\n", media);
+	printf("O desvio padrão é %f\n", desvio_padrao);
+
+}*/
