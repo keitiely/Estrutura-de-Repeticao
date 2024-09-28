464dcf4333f529115dfd8cacf12a73c81d7ffa8c
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-27 22:40:08 -0300

    Update Serie infinita

--- questao3.c
+++ questao3.c
@@ -1,4 +1,7 @@
-/*Questao 3*/
+/*Questao 3- Calcule o valor de a partir de uma série infinita
+	π = 4− 4/3 + 4/5 − 4/7 + 4/9 − 4/11 +... 
+Imprima uma tabela que mostre o valor aproximado de Pi calculando os 200.000 primeiros termos dessa série.
+Quantos termos você tem de utilizar antes de primeiro obter um valor que começa com 3,14159?*/
 
 #include <stdio.h>
 #include <math.h>
@@ -10,20 +13,13 @@ int main(){
 	printf("Digite as vezes que o programa vai rodar: ");
 	scanf("%d", &vezes);
 	
-		
 		for(debaixo=1; debaixo<vezes; debaixo+=2){
 		
 		x+= pow(-1, i)*(4.0/debaixo);
 		
 		i++;
 		}
-		
-		
-	
-		
 		printf("%.5f", x);
-	
-	
 	
 	 //+ou- 188000 vezes
 	return 0;
